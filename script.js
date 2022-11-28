74afca5f81a13ce18fef864a00c96d50136e48fe
Glitch (glitch-hello-website) <none>
2022-11-28 16:02:53 +0000

    📉👔 Checkpoint ./script.js:51566822/1262

--- script.js
+++ script.js
@@ -5,9 +5,30 @@ const graphics = new PIXI.Graphics();
 
 document.body.style.backgroundImage = "url('play-scene.png')"; 
 
-graphics.lineStyle(10, 0xFFBD01, 1);
-graphics.beginFill(0xC34288, 1);
+graphics.lineStyle(10, 0xFFFFFF, 1);
+graphics.beginFill(0,10, 1);
 graphics.drawCircle(400, 250, 50);
 graphics.endFill();
+const basicText = new PIXI.Text('Basic text in pixi');
+basicText.x = 50;
+basicText.y = 100;
 
+app.stage.addChild(basicText);
+
+const style = new PIXI.TextStyle({
+    fontFamily: 'Arial',
+    fontSize: 36,
+    fontStyle: 'italic',
+    fontWeight: 'bold',
+    fill: ['#ffffff', '#00ff99'], // gradient
+    stroke: '#4a1850',
+    strokeThickness: 5,
+    lineJoin: 'round',
+});
+
+const richText = new PIXI.Text('3', style);
+richText.x = 50;
+richText.y = 220;
+
+app.stage.addChild(richText);
 app.stage.addChild(graphics);
