ac8b5c84908271cceca7a659265ca6c0400eef7c
Glitch (glitch-hello-website) <none>
2022-11-28 16:59:55 +0000

    🚴🌔 Checkpoint ./script.js:51566822/1991

--- script.js
+++ script.js
@@ -4,32 +4,27 @@ document.body.appendChild(app.view);
 const graphics = new PIXI.Graphics();
 const bg = PIXI.Sprite.from('assets/play-scene.png');
 
-
 document.body.style.backgroundImage = "url('play-scene.png')"; 
 
+
 graphics.lineStyle(10, 0xFFFFFF, 1);
-graphics.beginFill(0,10, 1);
-graphics.drawCircle(400, 250, 50);
+graphics.beginFill(0, 50, 1);
+graphics.drawCircle(app.screen.width / 2, app.screen.height / 2 , 55);
 graphics.endFill();
 
-const basicText = new PIXI.Text('Numbers');
-basicText.x = 50;
-basicText.y = 100;
-
-app.stage.addChild(basicText);
 
 const style = new PIXI.TextStyle({
     fontFamily: 'Arial',
-    fontSize: 36,
+    fontSize: 60,
     fontWeight: 'bold',
     fill: ['#ffffff'],
-    strokeThickness: 15,
     lineJoin: 'round',
 });
 
 const richText = new PIXI.Text('3', style);
-richText.x = 50;
-richText.y = 220;
+richText.anchor.set(0.5);
+richText.x = app.screen.width / 2;
+richText.y = app.screen.height / 2;
 
 app.stage.addChild(richText);
 app.stage.addChild(graphics);
